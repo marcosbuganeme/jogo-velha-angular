0da7837548f912a4ccf2bca973128e878c1f6f9f
Marcos Buganeme <marcos.after@gmail.com>
2019-03-28 23:35:33 -0300

    criando componente de jogo da velha

--- src/app/jogo-da-velha/jogo-da-velha.module.ts
+++ src/app/jogo-da-velha/jogo-da-velha.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { JogoDaVelhaComponent } from './jogo-da-velha.component';
+
 @NgModule({
-  declarations: [],
+  declarations: [
+    JogoDaVelhaComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    JogoDaVelhaComponent
   ]
 })
 export class JogoDaVelhaModule { }
